056968aaf8545a0527869f71e62fee6712f57225
Votond <votond@outlook.com>
2026-07-22 21:52:07 +0300

    Refactor

--- src/generator.rs
+++ src/generator.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
 use anyhow::Result;
 use serde::Deserialize;
 use serde_json::Value;
-use std::fs::{read_to_string, File};
+use std::collections::HashMap;
+use std::fs::{File, read_to_string};
 use std::io::Write;
 use std::path::Path;
 
@@ -71,7 +71,8 @@ struct Method {
 
 pub fn generate_definitions(dump: &Path, output: &Path) -> Result<()> {
     let dump: Dump = serde_json::from_str(read_to_string(dump)?.as_str())?;
-    let mut types_mapping: HashMap<&String, &String> = HashMap::with_capacity(dump.type_count as usize);
+    let mut types_mapping: HashMap<&String, &String> =
+        HashMap::with_capacity(dump.type_count as usize);
     for t in &dump.types {
         types_mapping.insert(&t.clr_name, &t.lua_name);
     }
@@ -79,10 +80,22 @@ pub fn generate_definitions(dump: &Path, output: &Path) -> Result<()> {
         let mut definition = "---@meta\n\n\n---@class ".to_string();
         definition += format!("{}\n", t.lua_name).as_str();
         for field in &t.fields {
-            definition += format!("---@field {} {} {}\n", field.name, types_mapping.get_or_fallback(&field.r#type), get_field_description(field)).as_str();
+            definition += format!(
+                "---@field {} {} {}\n",
+                field.name,
+                types_mapping.get_or_fallback(&field.r#type),
+                get_field_description(field)
+            )
+            .as_str();
         }
         for property in &t.properties {
-            definition += format!("---@field {} {} {}\n", property.name, types_mapping.get_or_fallback(&property.r#type), get_property_description(property)).as_str();
+            definition += format!(
+                "---@field {} {} {}\n",
+                property.name,
+                types_mapping.get_or_fallback(&property.r#type),
+                get_property_description(property)
+            )
+            .as_str();
         }
         definition += (format!("{} = ", t.lua_name) + "{}\n\n").as_str();
         for constructor in &t.constructors {
@@ -90,19 +103,36 @@ pub fn generate_definitions(dump: &Path, output: &Path) -> Result<()> {
                 definition += handle_param(param, &types_mapping).as_str();
             }
             definition += format!("---@return {}\n", t.lua_name).as_str();
-            definition += format!("function {}.new({}) end\n\n", t.lua_name, get_method_params(&constructor.parameters)).as_str();
+            definition += format!(
+                "function {}.new({}) end\n\n",
+                t.lua_name,
+                get_method_params(&constructor.parameters)
+            )
+            .as_str();
         }
         for method in &t.methods {
             for param in &method.parameters {
                 definition += handle_param(param, &types_mapping).as_str();
             }
             if method.return_type != "void" {
-                definition += format!("---@return {}\n", types_mapping.get_or_fallback(&method.return_type)).as_str();
+                definition += format!(
+                    "---@return {}\n",
+                    types_mapping.get_or_fallback(&method.return_type)
+                )
+                .as_str();
             }
             let call_operator = if method.r#static { ":" } else { "." };
-            definition += format!("function {}{}{}({}) end\n\n", t.lua_name, call_operator, method.name, get_method_params(&method.parameters)).as_str();
+            definition += format!(
+                "function {}{}{}({}) end\n\n",
+                t.lua_name,
+                call_operator,
+                method.name,
+                get_method_params(&method.parameters)
+            )
+            .as_str();
         }
-        File::create(output.join(format!("{}.lua", t.lua_name)))?.write_all(definition.trim().as_bytes())?;
+        File::create(output.join(format!("{}.lua", t.lua_name)))?
+            .write_all(definition.trim().as_bytes())?;
     }
     Ok(())
 }
@@ -118,21 +148,48 @@ impl F for HashMap<&String, &String> {
 }
 
 fn handle_param(param: &Parameter, types_mapping: &HashMap<&String, &String>) -> String {
-    let param_type = if param.optional { &(param.r#type.clone() + "?") } else { &param.r#type.clone() };
-    format!("---@param {} {} {}\n", param.name, types_mapping.get_or_fallback(param_type), get_param_description(param))
+    let param_type = if param.optional {
+        &(param.r#type.clone() + "?")
+    } else {
+        &param.r#type.clone()
+    };
+    format!(
+        "---@param {} {} {}\n",
+        param.name,
+        types_mapping.get_or_fallback(param_type),
+        get_param_description(param)
+    )
 }
 
 const DESCRIPTION_DELIMITER: &str = " | ";
 
 fn get_field_description(field: &Field) -> String {
-    let is_static = if field.r#static { "Static field" } else { "Non-static field" };
-    let is_readonly = if field.read_only { "Read" } else { "Read/Write" };
+    let is_static = if field.r#static {
+        "Static field"
+    } else {
+        "Non-static field"
+    };
+    let is_readonly = if field.read_only {
+        "Read"
+    } else {
+        "Read/Write"
+    };
     format!("{is_static}{DESCRIPTION_DELIMITER}{is_readonly}")
 }
 
 fn get_property_description(property: &Property) -> String {
-    let is_static = if property.r#static { "Static property" } else { "Non-static property" };
-    let get_set = if property.can_read && property.can_write { "Read/Write" } else if property.can_read { "Read" } else { "Write" };
+    let is_static = if property.r#static {
+        "Static property"
+    } else {
+        "Non-static property"
+    };
+    let get_set = if property.can_read && property.can_write {
+        "Read/Write"
+    } else if property.can_read {
+        "Read"
+    } else {
+        "Write"
+    };
     format!("{is_static}{DESCRIPTION_DELIMITER}{get_set}")
 }
 
